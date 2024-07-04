61b78d1a3d075362f344118cf7f2148a31156b05
Siddharth Prakash <iamsid0011@gmail.com>
2024-07-04 10:16:13 +0530

    commit

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,28 +1,41 @@
 import os
-from langchain.llms import Ollama
-from langchain.chains import LLMChain
+from langchain_community.llms import Ollama
 from langchain.prompts import PromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import RunnablePassthrough
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from dotenv import load_dotenv
 
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
-if __name__ == "__main__":
-    print("Hello Langchain!")
     summary_template = """
-        given the linkedin information {information} about a person from I want you to create:
-        1) a short summary
-        2) two interesting facts about them
+    Given the LinkedIn information {information} about a person, I want you to create:
+    1) a short summary
+    2) two interesting facts about them
     """
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
-    
+
     # Initialize Ollama with the Llama 2 model
     llm = Ollama(model="llama3")
-    
-    # Now we want to tie everything together
-    chain = LLMChain(prompt=summary_prompt_template, llm=llm)
-    
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/siddharth-prakash-771596241/")
-    res = chain.invoke(input={"information": linkedin_data})
-    print(res)
+
+    # Create a RunnableSequence instead of LLMChain
+    chain = (
+        {"information": RunnablePassthrough()} 
+        | summary_prompt_template 
+        | llm 
+        | StrOutputParser()
+    )
+
+    res = chain.invoke(linkedin_data)
+    return res
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice Breaker")
+    result = ice_break_with(name="Siddharth Prakash PES University")
+    print(result)
--- ice_breaker/third_parties/linkedin.py
+++ ice_breaker/third_parties/linkedin.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 # Load environment variables from a .env file
 load_dotenv()
 
-def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = True):
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     """
     Scrape information from LinkedIn profiles using Proxycurl API.
     """
